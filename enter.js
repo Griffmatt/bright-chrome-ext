6d67f857df8cedd9fdea9f6f056344fa6bc18f25
Griffin Matthews <68610081+Griffmatt@users.noreply.github.com>
2023-03-30 10:28:27 -0400

    Update enter.js

--- enter.js
+++ enter.js
@@ -1,1 +1,9 @@
+function simulateKey(view, keyCode, key) {
+  let event = document.createEvent("Event")
+  event.initEvent("keydown", true, true)
+  event.keyCode = keyCode
+  event.key = event.code = key
+  return view.someProp("handleKeyDown", f => f(view, event))
+}
 
+simulateKey(view, 13, "Enter")
